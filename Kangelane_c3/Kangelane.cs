305c7fea15edd28ebb020afc2c0891443f5a0159
AnTu111 <85964940+AnTu111@users.noreply.github.com>
2024-05-09 01:43:57 +0300

    Add files via upload

---
+++ Kangelane_c3/Kangelane.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    class Kangelane
+    {
+
+        private string _Nimi;
+        private string _Asukoht;
+        public Kangelane(string Nimi, string Asukoht)
+        {
+            this.Nimi = Nimi;
+            this.Asukoht = Asukoht;
+        }
+
+        public string Nimi //свойство 
+        {
+            get { return _Nimi; } //методы.вызывается при попытке получить значение свойства Nimi.
+                                  // возвращает значение приватного поля _Nimi.
+            set
+            {
+                _Nimi = value;
+            }
+        }
+        public string Asukoht 
+        { 
+            get { return _Asukoht; }
+
+            set 
+            { 
+                _Asukoht = value; 
+            }
+        }
+        public virtual int Päästa(int Ohustatuid) //переопределяется в производных классах
+        {
+            return (int)Math.Round(Ohustatuid * 0.95, 0);
+        }
+        public override string ToString()
+        {
+            return Nimi + " kaitseb " + Asukoht;
+        }
+    }
+}
---
+++ Kangelane_c3/SuperKangelane.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    class SuperKangelane : Kangelane
+    {
+        private static readonly Random rand = new Random();
+        private double Osavus = Math.Round((rand.NextDouble() * 4) + 1);
+        public double _Osavus
+        {
+            get { return Osavus; }
+            set { Osavus = value; }
+        }
+
+        public SuperKangelane(string Nimi, string Asukoht) : base(Nimi, Asukoht) //пердает классу Kangelane
+        {
+        }
+        public override int Päästa(int ohustatuid)
+        {
+            return (int)Math.Round((95 + _Osavus) / 100 * ohustatuid, 0);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " ja ta on selles " + _Osavus + " protsenti parem kui lihtsalt kangelased";
+        }
+    }
+}
